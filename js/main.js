d838e1c54e92fe91a531aa8a9db3b802504a8085
Iryna Lipska <iranion.L@gmail.com>
2025-02-09 18:00:33 +0200

    Add first js

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let textHello = "Hello";
+
+console.log(textHello);
+
+let userName = prompt("Enter your name");
+console.log(userName);
+alert(`Hello , ${userName}`);
+
+const today = 2025;
+let userBorn = prompt("When you was born?");
+console.log(today - userBorn);
+alert(`You are ${today - userBorn} years old`);
+
+let side = prompt(`enter the length of the side of the square:`);
+alert(`The perimetr of the square is ${side * 4}`);
